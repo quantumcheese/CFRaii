d668571c23d80d9873d8718204542bd7fee66af3
quantumcheese <rabrown@quantumcheese.com>
2010-07-20 11:15:04 -0400

    Added mutable / immutable dictionary data members.  Made proxy objects for operator [].  Fixed up copying and operators.

--- QCDictionary.h
+++ QCDictionary.h
@@ -19,60 +19,96 @@
 
 class QCDictionary
 {
-	CFMutableDictionaryRef dict;
+	CFMutableDictionaryRef	mDict;
+	CFDictionaryRef			dict;
 	
-	CFMutableDictionaryRef CFMutableDictionaryFromCFDictionary(CFDictionaryRef const &inDict) const
+	CFMutableDictionaryRef CFMutableDictionaryFromCFDictionary(CFDictionaryRef const inDict) const
 	{
+		return (inDict == NULL) ? NULL : CFDictionaryCreateMutableCopy(kCFAllocatorDefault, 0, inDict);
+#if 0
 		CFMutableDictionaryRef temp(0);
 		if (inDict != 0)
 		{
 			temp = CFDictionaryCreateMutableCopy(kCFAllocatorDefault, 0, inDict);	
 		}
 		return temp;
+#endif
 	}
 	
 public:
 	QCDictionary( )
-	: dict( CFDictionaryCreateMutable(kCFAllocatorDefault, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks) )
+	: dict( NULL)
+	, mDict( CFDictionaryCreateMutable(kCFAllocatorDefault, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks) )
 	{ }
 	
 	QCDictionary(CFMutableDictionaryRef const &inDict)
-	: dict( inDict )
-	{
-		// do not release the dictionary!
-	}
+	: dict( NULL)
+	, mDict( inDict )
+	{ }
 	
 	QCDictionary(CFDictionaryRef const &inDict)
-	: dict( CFMutableDictionaryFromCFDictionary(inDict) )
-	{
-		// because we're 'taking ownership' of this dictionary, release the original
-		QCRelease(inDict);
-	}
+	: dict( inDict )
+	, mDict( NULL )
+	{ }
 	
 	// copy constructor
 	QCDictionary(QCDictionary const &inDict)
 	: dict( QCRetain(inDict.dict) )
+	, mDict( QCRetain(inDict.mDict) )
 	{ }
 	
 	// destructor
 	~QCDictionary( )
 	{
 		QCRelease(dict);
+		QCRelease(mDict);
+	}
+	
+	CFDictionaryRef Dictionary () const
+	{
+		return isNotNull(mDict) ? mDict : dict;
+	}
+	
+	CFDictionaryRef CFDictionary () const
+	{
+		return isNotNull(dict) ? dict : mDict;
+	}
+	
+	void makeMutable()
+	{
+		if (mDict == NULL)
+		{
+			if (dict != NULL)
+			{
+				mDict = CFMutableDictionaryFromCFDictionary(dict);
+				QCRelease(dict);
+				dict = NULL;
+			}
+			else
+			{
+				mDict = CFDictionaryCreateMutable(kCFAllocatorDefault
+												  , 0
+												  , &kCFTypeDictionaryKeyCallBacks
+												  , &kCFTypeDictionaryValueCallBacks);
+			}
+
+		}
 	}
 	
 	void makeUnique()
 	{
-		if (! null() && CFGetRetainCount(dict) > 1)
+		if (! null() && CFGetRetainCount(Dictionary()) > 1)
 		{
-			CFMutableDictionaryRef newDict = CFDictionaryCreateMutableCopy(kCFAllocatorDefault, 0, dict);
-			QCRelease(dict);
-			dict = newDict;
+			CFMutableDictionaryRef newDict = CFDictionaryCreateMutableCopy(kCFAllocatorDefault, 0, Dictionary());
+			QCRelease(Dictionary());
+			dict = NULL;
+			mDict = newDict;
 		}
 	}
 	
 	bool null() const
 	{
-		return dict == 0;
+		return isNull(Dictionary());
 	}
 	
 	CFIndex count() const
@@ -87,26 +123,18 @@ public:
 	
 	QCDictionary copy() const
 	{
-		return null() ? QCDictionary() : QCDictionary(CFDictionaryCreateMutableCopy(kCFAllocatorDefault, 0, dict));
+		return QCDictionary(*this); // null() ? QCDictionary() : QCDictionary(CFDictionaryCreateMutableCopy(kCFAllocatorDefault, 0, dict));
 	}
 	
-	// Operators
+	// operators
 	
-	// assignment
-	QCDictionary & operator = (QCDictionary &rhs)	
+	// copy assignment
+	QCDictionary & operator = (QCDictionary const &rhs)	
 	{
-		if (dict != rhs.dict)
-		{
-			QCRelease(dict);
-			dict = QCRetain(rhs.dict);
-		}
-		return *this;
-	}
-	
-	QCDictionary & operator = (QCDictionary const &rhs)
-	{
+		// copy and swap
 		QCDictionary temp(rhs);
 		std::swap(dict, temp.dict);
+		std::swap(mDict, temp.mDict);
 		return *this;
 	}
 	
@@ -114,8 +142,8 @@ public:
 	
 	bool operator == (QCDictionary const &rhs) const
 	{
-		return (dict == rhs.dict)
-				|| (CFEqual(dict, rhs.dict) == true);
+		return (Dictionary() == rhs.Dictionary())
+				|| (CFEqual(Dictionary(), rhs.Dictionary()) == true);
 	}
 	
 	bool operator != (QCDictionary const &rhs) const
@@ -123,50 +151,140 @@ public:
 		return !(*this == rhs);
 	}
 	
+#pragma mark class CFTypeProxy
+	class CFTypeProxy
+	{
+	private:
+		CFDictionaryRef	dict;
+		CFTypeRef		key;
+		
+		// no copy-assigmnet
+		CFTypeProxy & operator = (CFTypeProxy const &);
+		
+	public:
+		// ctor
+		CFTypeProxy(CFDictionaryRef const dictRef, CFTypeRef const keyRef)
+		: dict( QCRetain(dictRef) ), key( QCRetain(keyRef) )
+		{ }
+		
+		// copy ctor
+		CFTypeProxy(CFTypeProxy const &proxy)
+		: dict( QCRetain(proxy.dict) ), key( QCRetain(proxy.key) )
+		{ }
+		
+		// dtor
+		~CFTypeProxy()
+		{
+			QCRelease(dict);
+			QCRelease(key);
+		}
+		
+		// comparison operators
+		bool operator == (CFTypeProxy const &rhs) const
+		{
+			// could use CFEqual(array, rhs.array)
+			return dict == rhs.dict && key == rhs.key;
+		}
+		
+		bool operator != (CFTypeProxy const &rhs) const
+		{
+			return !(*this == rhs);
+		}
+		
+		// conversion operator
+		operator CFTypeRef () const
+		{
+			return CFDictionaryGetValue(dict, key);
+		}
+	}; // class CFTypeProxy
+	
+#pragma mark class CFMutableTypeProxy
+	class CFMutableTypeProxy
+	{
+	private:
+		CFMutableDictionaryRef	dict;
+		CFTypeRef				key;
+		
+		// no copy-assignment
+		CFMutableTypeProxy & operator = (CFMutableTypeProxy const &);
+		
+	public:
+		// ctor
+		CFMutableTypeProxy(CFMutableDictionaryRef const dictRef, CFTypeRef const keyRef)
+		: dict( QCRetain(dictRef) ), key( QCRetain(keyRef) )
+		{ }
+		
+		// copy ctor
+		CFMutableTypeProxy(CFMutableTypeProxy const &proxy)
+		: dict( QCRetain(proxy.dict) ), key( QCRetain(proxy.key) )
+		{ }
+		
+		// dtor
+		~CFMutableTypeProxy()
+		{
+			QCRelease(dict);
+			QCRelease(key);
+		}
+		
+		// conversion operator
+		operator CFTypeRef () const
+		{
+			return CFDictionaryGetValue(dict, key);
+		}
+		
+		// assignment replaces or adds an element in the dictionary
+		CFMutableTypeProxy & operator = (CFTypeRef const newValue)
+		{
+			// 'this' doesn't change, but the pointed-to dictionary does
+			// "add if absent, replace if present"
+			CFDictionarySetValue(dict, key, newValue);
+			return *this;
+		}
+	}; // class CFMutableTypeProxy
+	
 	// accessors
+	
+	CFMutableTypeProxy operator [] (CFTypeRef const key)
+	{
+		makeMutable();
+		return CFMutableTypeProxy(mDict, key);
+	}
 	
 	CFTypeRef operator [] (CFTypeRef const key) const
 	{
-		return (*this)(key);
-		return null() ? 0 : CFDictionaryGetValue(dict, key);
+#if 0
+		return (*this)(key); // see below
+//		return CFTypeProxy(Dictionary(), key);
 	}
 	
 	CFTypeRef operator () (CFTypeRef const key) const
 	{
-		return (null() || key == 0)
-		? 0
-		: CFDictionaryGetValue(dict, key);
+#endif
+		return (null() || key == NULL)
+		? NULL
+		: CFDictionaryGetValue(Dictionary(), key);
 	}
-	
+#if 0
 	void operator () (CFTypeRef const key, CFTypeRef const value)
 	{
-		if (!null())
-		{
-			makeUnique();
-			CFDictionarySetValue(dict, key, value);
-		}
+		makeUnique(); // also makes mutable
+		CFDictionarySetValue(mDict, key, value);
 	}
-	
+#endif
 	void addValue(CFTypeRef const key, CFTypeRef const value)
 	{
 		makeUnique();
-		CFDictionaryAddValue(dict, key, value);
+		CFDictionaryAddValue(mDict, key, value);
 	}
 	void setValue(CFTypeRef const key, CFTypeRef const value)
 	{
 		makeUnique();
-		CFDictionarySetValue(dict, key, value);
-	}
-	
-	// conversion operators
-	operator CFMutableDictionaryRef ()
-	{
-		return dict;
+		CFDictionarySetValue(mDict, key, value);
 	}
 	
 	operator CFDictionaryRef () const
 	{
-		return dict;
+		return Dictionary();
 	}
 	
 	void show() const;
