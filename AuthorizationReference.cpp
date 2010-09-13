103bdea4e6ea6bdfc457959b85e364e82ac0f61c
quantumcheese <rabrown@quantumcheese.com>
2010-09-13 13:51:54 -0400

    Adding files for Authorization services wrappers (for exception safety, etc.).

---
+++ AuthorizationReference.cpp
@@ -0,0 +1,11 @@
+/*
+ *  AuthorizationReference.cpp
+ *  CFRaii
+ *
+ *  Created by Richard Brown on 9/13/10.
+ *  Copyright 2010 Javacool Software Development, LLC. All rights reserved.
+ *
+ */
+
+#include "AuthorizationReference.h"
+
