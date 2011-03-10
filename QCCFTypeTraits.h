b0e4b40c02feef5a99a495de2a90cefe91526707
Richard <rabrown@quantumcheese.com>
2011-03-09 22:55:11 -0500

    Lots of fixes to type_traits system.  Now using tr1::true_type and (tr1::false_type).  Also have CFType_traits struct up and running (i.e., it compiles); not sure yet how to use it.  (We're just imitating code examples so far.)

--- QCCFTypeTraits.h
+++ QCCFTypeTraits.h
@@ -13,254 +13,261 @@
 
 #include <tr1/type_traits>
 
+#include "CFRaiiCommon.h"
+
 // MARK: -
 // MARK: Is Core Foundation type
 
-template <class >
-struct is_CFType
-{ static bool const value = false; };
+// MARK: internal template & specializations
+template <class T>
+struct _is_CFType : public std::tr1::false_type
+{ };
 
 template <>
-struct is_CFType <CFAllocatorRef>
-{ static bool const value = true; };
+struct _is_CFType <CFAllocatorRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFArrayRef>
-{ static bool const value = true; };
+struct _is_CFType <CFArrayRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFAttributedStringRef>
-{ static bool const value = true; };
+struct _is_CFType <CFAttributedStringRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFBagRef>
-{ static bool const value = true; };
+struct _is_CFType <CFBagRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFBinaryHeapRef>
-{ static bool const value = true; };
+struct _is_CFType <CFBinaryHeapRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFBitVectorRef>
-{ static bool const value = true; };
+struct _is_CFType <CFBitVectorRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFBooleanRef>
-{ static bool const value = true; };
+struct _is_CFType <CFBooleanRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFBundleRef>
-{ static bool const value = true; };
+struct _is_CFType <CFBundleRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFCalendarRef>
-{ static bool const value = true; };
+struct _is_CFType <CFCalendarRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFCharacterSetRef>
-{ static bool const value = true; };
+struct _is_CFType <CFCharacterSetRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFDataRef>
-{ static bool const value = true; };
+struct _is_CFType <CFDataRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFDateRef>
-{ static bool const value = true; };
+struct _is_CFType <CFDateRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFDateFormatterRef>
-{ static bool const value = true; };
+struct _is_CFType <CFDateFormatterRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFDictionaryRef>
-{ static bool const value = true; };
+struct _is_CFType <CFDictionaryRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFErrorRef>
-{ static bool const value = true; };
+struct _is_CFType <CFErrorRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFFileDescriptorRef>
-{ static bool const value = true; };
+struct _is_CFType <CFFileDescriptorRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFLocaleRef>
-{ static bool const value = true; };
+struct _is_CFType <CFLocaleRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFMachPortRef>
-{ static bool const value = true; };
+struct _is_CFType <CFMachPortRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFMessagePortRef>
-{ static bool const value = true; };
+struct _is_CFType <CFMessagePortRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFMutableArrayRef>
-{ static bool const value = true; };
+struct _is_CFType <CFMutableArrayRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFMutableAttributedStringRef>
-{ static bool const value = true; };
+struct _is_CFType <CFMutableAttributedStringRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFMutableBagRef>
-{ static bool const value = true; };
+struct _is_CFType <CFMutableBagRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFMutableBitVectorRef>
-{ static bool const value = true; };
+struct _is_CFType <CFMutableBitVectorRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFMutableCharacterSetRef>
-{ static bool const value = true; };
+struct _is_CFType <CFMutableCharacterSetRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFMutableDataRef>
-{ static bool const value = true; };
+struct _is_CFType <CFMutableDataRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFMutableDictionaryRef>
-{ static bool const value = true; };
+struct _is_CFType <CFMutableDictionaryRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFMutableSetRef>
-{ static bool const value = true; };
+struct _is_CFType <CFMutableSetRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFMutableStringRef>
-{ static bool const value = true; };
+struct _is_CFType <CFMutableStringRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFNotificationCenterRef>
-{ static bool const value = true; };
+struct _is_CFType <CFNotificationCenterRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFNullRef>
-{ static bool const value = true; };
+struct _is_CFType <CFNullRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFNumberRef>
-{ static bool const value = true; };
+struct _is_CFType <CFNumberRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFNumberFormatterRef>
-{ static bool const value = true; };
+struct _is_CFType <CFNumberFormatterRef> : public std::tr1::true_type
+{ };
 
 #if 0
 // CFPluginRef is typedef'd to the same opaque type as CFBundleRef
 template <>
-struct is_CFType <CFPlugInRef>
-{ static bool const value = true; };
+struct _is_CFType <CFPlugInRef> : public std::tr1::true_type
+{ };
 #endif
 
 template <>
-struct is_CFType <CFPlugInInstanceRef>
-{ static bool const value = true; };
+struct _is_CFType <CFPlugInInstanceRef> : public std::tr1::true_type
+{ };
 
 #if 0
 // CFTypeRef and CFPropertyListRef are both typedefs for void const *
 template <>
-struct is_CFType <CFPropertyListRef>
-{ static bool const value = true; };
+struct _is_CFType <CFPropertyListRef> : public std::tr1::true_type
+{ };
 #endif
 
 template <>
-struct is_CFType <CFReadStreamRef>
-{ static bool const value = true; };
+struct _is_CFType <CFReadStreamRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFRunLoopRef>
-{ static bool const value = true; };
+struct _is_CFType <CFRunLoopRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFRunLoopObserverRef>
-{ static bool const value = true; };
+struct _is_CFType <CFRunLoopObserverRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFRunLoopSourceRef>
-{ static bool const value = true; };
+struct _is_CFType <CFRunLoopSourceRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFRunLoopTimerRef>
-{ static bool const value = true; };
+struct _is_CFType <CFRunLoopTimerRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFSetRef>
-{ static bool const value = true; };
+struct _is_CFType <CFSetRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFSocketRef>
-{ static bool const value = true; };
+struct _is_CFType <CFSocketRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFStringRef>
-{ static bool const value = true; };
+struct _is_CFType <CFStringRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFStringTokenizerRef>
-{ static bool const value = true; };
+struct _is_CFType <CFStringTokenizerRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFTimeZoneRef>
-{ static bool const value = true; };
+struct _is_CFType <CFTimeZoneRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFTreeRef>
-{ static bool const value = true; };
+struct _is_CFType <CFTreeRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFTypeRef>
-{ static bool const value = true; };
+struct _is_CFType <CFTypeRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFURLRef>
-{ static bool const value = true; };
+struct _is_CFType <CFURLRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFUserNotificationRef>
-{ static bool const value = true; };
+struct _is_CFType <CFUserNotificationRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFUUIDRef>
-{ static bool const value = true; };
+struct _is_CFType <CFUUIDRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFWriteStreamRef>
-{ static bool const value = true; };
+struct _is_CFType <CFWriteStreamRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFXMLNodeRef>
-{ static bool const value = true; };
+struct _is_CFType <CFXMLNodeRef> : public std::tr1::true_type
+{ };
 
 template <>
-struct is_CFType <CFXMLParserRef>
-{ static bool const value = true; };
+struct _is_CFType <CFXMLParserRef> : public std::tr1::true_type
+{ };
 
 #if 0
 // CFXMLTreeRef is a typedef for CFTreeRef
 template <>
-struct is_CFType <CFXMLTreeRef>
-{ static bool const value = true; };
+struct _is_CFType <CFXMLTreeRef> : public std::tr1::true_type
+{ };
 #endif
 
 
-#if 0
+// MARK: public-facing is_CFType<> interface
+template <class T>
+struct is_CFType : public _is_CFType<typename std::tr1::remove_cv<T>::type>
+{ };
+
+
 // MARK: -
 // MARK: CFType_traits
 
 // only implement CFType_traits if is_CFType is true
-template <class CF, bool = is_CFType<CF>::value>
+template < class CF, bool = is_CFType<CF>::value >
 struct CFType_traits;
 
-template <class CF, true>
-struct CFType_traits
+template <class CF>
+struct CFType_traits <CF, true>
 {
 	static bool const is_CFType = true;
-	typedef CF type;
-	static type get(CF const &obj) { return obj; }
+	typedef CF value_type;
+	static value_type get(CF const &obj) { return obj; }
 };
-#endif
 
 #endif
