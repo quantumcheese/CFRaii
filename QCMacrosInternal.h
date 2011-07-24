ba19031aa96898179f70487ffa192792fdc76acc
Richard <rabrown@quantumcheese.com>
2011-07-24 11:17:40 -0400

    Tweaked namespace macros.

--- QCMacrosInternal.h
+++ QCMacrosInternal.h
@@ -10,9 +10,17 @@
 #ifndef _QC_MACROS_INTERNAL_
 #define _QC_MACROS_INTERNAL_
 
+#ifndef BEGIN_NAMESPACE
+#define BEGIN_NAMESPACE(x) namespace x {
+#endif
+#ifndef END_NAMESPACE
+#define END_NAMESPACE }
+#endif
+
+
 #if 1
-	#define BEGIN_QC_NAMESPACE	namespace QC {
-	#define END_QC_NAMESPACE	} // namespace QC
+	#define BEGIN_QC_NAMESPACE	BEGIN_NAMESPACE(QC)
+	#define END_QC_NAMESPACE	END_NAMESPACE
 #else
 	#define BEGIN_QC_NAMESPACE
 	#define END_QC_NAMESPACE
