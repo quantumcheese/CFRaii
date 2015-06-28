a40fc9f0e272d8efd0b0a4d7cf284a82218a1b73
Richard A. Brown <rabrown@quantumcheese.com>
2015-06-28 15:55:05 -0400

    Convert tabs to spaces; clarify return values.

--- QCString.cpp
+++ QCString.cpp
@@ -15,70 +15,70 @@ BEGIN_QC_NAMESPACE
 
 char *QCString::CString() const
 {
-	return CString_malloc();
+    return CString_malloc();
 }
 
-// returns a C-string that requires free()ing
+// returns a C-string that requires free()-ing, or NULL.
 char *QCString::CString_malloc() const
 {
-	if (null()) return NULL;
-	
-	CFIndex length = CFStringGetMaximumSizeOfFileSystemRepresentation(String());
-	char *buffer = static_cast<char *> (malloc(length * sizeof(char)));
-	
-	if (CFStringGetFileSystemRepresentation(String(), buffer, length) == true)
-	{
-		return buffer;
-	}
-	// fall-through failure case
-	free(buffer);
-	return NULL;
+    if (null()) { return NULL; }
+
+    CFIndex length = CFStringGetMaximumSizeOfFileSystemRepresentation(String());
+    char *buffer = static_cast<char *> (malloc(length * sizeof(char)));
+
+    if (CFStringGetFileSystemRepresentation(String(), buffer, length) == true)
+    {
+        return buffer;
+    }
+    // fall-through failure case
+    free(buffer);
+    return NULL;
 }
 
-// returns a C-string that requires delete[]-ing or NULL
+// returns a C-string that requires delete[]-ing, or NULL
 char *QCString::CString_new() const
 {
-	if (null()) return NULL;
-	
-	CFIndex length = CFStringGetMaximumSizeOfFileSystemRepresentation(String());
-	char *buffer = new char[length];
-	
-	if (CFStringGetFileSystemRepresentation(String(), buffer, length) == true)
-	{
-		return buffer;
-	}
-	// fall-through failure case
-	delete [] buffer;
-	return NULL;
+    if (null()) { return NULL; }
+
+    CFIndex length = CFStringGetMaximumSizeOfFileSystemRepresentation(String());
+    char *buffer = new char[length];
+
+    if (CFStringGetFileSystemRepresentation(String(), buffer, length) == true)
+    {
+        return buffer;
+    }
+    // fall-through failure case
+    delete [] buffer;
+    return NULL;
 }
 
 std::string QCString::StdString() const
 {
-	// not very efficient
-	char * const cString = CString_new();
-	std::string const stdStr(cString);
-	delete cString;
-	
-	return stdStr;
+    // not very efficient
+    char * const cString = CString_new();
+    std::string const stdStr(cString);
+    delete cString;
+
+    return stdStr;
 }
 
 void QCString::show() const
 {
 #ifndef NDEBUG
-	CFShow(String());
+    CFShow(String());
 #endif
 }
 
 bool QCString::writeToFile(QCString const &filePath) const
 {
-	char *buffer = CString();
-	
-	QCData bytes( CFDataCreateWithBytesNoCopy(kCFAllocatorDefault, reinterpret_cast<UInt8 *> (buffer), length(), kCFAllocatorNull) );
-	bool written = bytes.writeToFile(filePath);
-	
-	free(buffer);
-	
-	return written;
+    char *buffer = CString();
+
+    QCData bytes( CFDataCreateWithBytesNoCopy(kCFAllocatorDefault, reinterpret_cast<UInt8 *> (buffer), length(), kCFAllocatorNull) );
+    bool written = bytes.writeToFile(filePath);
+
+    free(buffer);
+
+    return written;
 }
 
 END_QC_NAMESPACE
